7f754b1a1ed1d86c325fcc2a485d36a6f8c2fff6
bigbro <bigbrodeveloper@gmail.com>
2020-12-08 18:15:31 +0900

    wefwef

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
-var port = 3000;
+var port = 32319;
 
 
 server.on('listening', function () {
